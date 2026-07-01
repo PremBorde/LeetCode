eedd02e658500b0a72455ea5e1e201e4546ccb08
Prem Borde <prembordesn4@gmail.com>
2026-07-01 22:10:14 +0530

    Time: 12 ms (28.4%), Space: 19.4 MB (17.89%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        temp = x    # Kepp the original Number
+        rev = 0    
+        while x > 0:
+            digit = x % 10   # Last digit Extract
+            rev = rev * 10 + digit     # Build Reverse the number 
+            x //= 10   # Remove last digit x
+
+        return temp == rev
